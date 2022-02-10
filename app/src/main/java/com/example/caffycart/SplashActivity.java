ace7925c120be69578c565c6f03278bb7dea47f8
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-02-10 11:39:56 +0530

    Login UI done, signup UI started

--- app/src/main/java/com/example/caffycart/SplashActivity.java
+++ app/src/main/java/com/example/caffycart/SplashActivity.java
@@ -43,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
                 startActivity(intent);
                 finish();
             }
